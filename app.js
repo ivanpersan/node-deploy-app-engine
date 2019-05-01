64e9effeb072375dffb31ae42e56cc8a674266ec
Ivan <ivanperez1986@gmail.com>
2019-05-01 12:31:35 +0200

    Working with example.org in remote

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const puppeteer = require('puppeteer');
 
-app.post('/', (req, res) => {
-  res.send("My response");
+app.post('/', async (req, res) => {
+  const browser = await puppeteer.launch({headless: true, args:['--no-sandbox']});
+  const page = await configurePage(browser);
+  await page.goto('http://www.example.org/');  
+  const imageBuffer = await page.screenshot();
+  browser.close(); 
+  res.set('Content-Type', 'image/png');
+  res.send(imageBuffer);
 });
 
 const server = app.listen(process.env.PORT || 8080, err => {
@@ -10,3 +17,10 @@ const server = app.listen(process.env.PORT || 8080, err => {
   const port = server.address().port;
   console.info(`App listening on port ${port}`);
 });
+
+async function configurePage(browser) {
+  let defaultUserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36';
+  const page = await browser.newPage();
+  page.setUserAgent(defaultUserAgent)
+  return page;
+}
